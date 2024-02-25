44ca32b90301b23b349020c4b8d6dddc52879eb1
roangoulart <roangoulart@gmail.com>
2024-02-25 00:31:21 -0300

    Finalizado estruturação do código, estilização e scripts. O app está com scripts e funcionalidades totalmente funcionais. Estilização e decoração simples e intuitivo de como usar.

--- js/script.js
+++ js/script.js
@@ -1,53 +1,58 @@
-const content = document.querySelector(".conteudo");
-const btnNew = document.querySelector(".criar-nota")
+
+const conteudo = document.querySelector(".conteudo");
+const addNota = document.querySelector(".criarNota");
 
 let items_db = localStorage.getItem("items_db")
-    ? JSON.parse(localStorage.getItem("items_db")) : [];
+    ? JSON.parse(localStorage.getItem("items_db"))
+    : [];
 
-const color = [
-    "gray",
+const cores = [
     "red",
     "blue",
     "yellow",
+    "green",
     "orange",
     "purple",
 ];
 
-const cor = () => color[Math.floor(Math.random() * VideoColorSpace.length)];
+const corRandom = () => cores[Math.floor(Math.random() * cores.length)];
 
 function carregaItem() {
-    content.innerHTML = "";
+    conteudo.innerHTML = "";
     verifyNulls();
 
     items_db.forEach((item) => {
         addHTML(item);
     });
+    addEvents();
 }
 
-btnNew.onclick = () => {
+addNota.onclick = () => {
     addHTML();
-}
+
+    addEvents();
+};
 
 function addHTML(item) {
     const nav = document.createElement("nav");
 
-    nav.innerHTML = `<nav class="item" style="background-color: ${item?.color || cor()}">
+    nav.innerHTML = `<nav class="item" style="background-color: ${item?.cores || corRandom()
+        }">
     <span class="remove">X</span>
-    <textarea>$(item?.text || "")</textarea>
-    </nav>`;
+    <textarea>${item?.text || ""}</textarea>`;
 
-    content.appendChild(nav);
+    conteudo.appendChild(nav);
 }
 
 function addEvents() {
     const notas = document.querySelectorAll(".item textarea");
     const remove = document.querySelectorAll(".item .remove");
 
-    notas.forEach((item) => {
+    notas.forEach((item, i) => {
         item.oninput = () => {
             items_db[i] = {
                 text: item.value,
-                color: items_db[i]?.color || item.parentElement.style.background,
+                cores: items_db[i]?.cores || item.parentElement.style.backgroundColor,
             };
 
             localStorage.setItem("items_db", JSON.stringify(items_db));
@@ -56,7 +61,7 @@ function addEvents() {
 
     remove.forEach((item, i) => {
         item.onclick = () => {
-            content.children[i].remove();
+            conteudo.children[i].remove();
             if (items_db[i]) {
                 items_db.splice(i, 1);
                 localStorage.setItem("items_db", JSON.stringify(items_db));
@@ -71,4 +76,4 @@ function verifyNulls() {
     localStorage.setItem("item_db", JSON.stringify(items_db));
 }
 
-loadItems();
+carregaItem();
